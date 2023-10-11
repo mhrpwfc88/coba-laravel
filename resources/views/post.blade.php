6d58df48eb871644fe68c0bd040109304ac00031
mhrpwfc88 <mh17pwfc@gmail.com>
2023-10-11 21:17:05 +0700

    update

---
+++ resources/views/post.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('container')
+<article>
+    <h2>judul</h2>
+    <h5>author</h5>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium expedita eum dicta cum. Architecto quas ipsam id nesciunt animi asperiores error, sed nobis fugiat officiis commodi saepe, ratione quia autem!</p>
+</article>
+@endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,8 +7,10 @@
 <h1>posts</h1>
     @foreach ($posts as $post)
     <article class="mb-5">
-        <h2>{{ $post["title"] }}</h2>
-        <h5>{{ $post["author"] }}</h5>
+        <h2>
+            <a href="/posts/{{ $post["slug"] }}">{{ $post["title"] }}</a>
+        </h2>
+        <h5>By: {{ $post["author"] }}</h5>
         <p>{{ $post["body"] }}</p>
     </article>
     @endforeach
